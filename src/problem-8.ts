bcbb7352992f27d4359c04ff23c5be60e9ca4a42
alamin6688 <alamin.kzs06@gmail.com>
2024-11-08 02:49:10 +0600

    Problem-8 Sloved

---
+++ src/problem-8.ts
@@ -0,0 +1,17 @@
+{
+  //
+  // Problem 08 Soluton:-
+
+  function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
+    return keys.every((key) => key in obj);
+  }
+
+  const person = { name: "Alice", age: 28, email: "alice@example.com" };
+
+  const Output1 = validateKeys(person, ["name", "age"]);
+  //   console.log(Output1);
+
+  const Output2 = validateKeys(person, ["name", "address"]);
+  //   console.log(Output2);
+  //
+}
