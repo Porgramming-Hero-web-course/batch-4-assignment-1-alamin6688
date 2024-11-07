9f17868dd671d8fc5afb0865a8e69423d4d7468d
alamin6688 <alamin.kzs06@gmail.com>
2024-11-08 01:21:08 +0600

    Problem-2 Sloved

---
+++ src/problem-2.ts
@@ -0,0 +1,12 @@
+{
+  // Problem 02 Soluton:-
+
+  const removeDuplicates = (numbers: number[]): number[] => {
+    return [...new Set(numbers)];
+  };
+
+  const Output = removeDuplicates([1, 2, 2, 3, 3, 4, 4, 5, 6, 88, 88]);
+  //   console.log(Output);
+
+  //
+}
