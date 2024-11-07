265c2628ac220867233dbc3c4c9fc5c616a4b06c
alamin6688 <alamin.kzs06@gmail.com>
2024-11-08 01:51:24 +0600

    Problem-4 Sloved

---
+++ src/problem-4.ts
@@ -0,0 +1,35 @@
+{
+  //
+  // Problem 04 Soluton:-
+
+  type Circle = {
+    shape: "circle";
+    radius: number;
+  };
+
+  type Rectangle = {
+    shape: "rectangle";
+    width: number;
+    height: number;
+  };
+
+  const calculateShapeArea = (shape: Circle | Rectangle): number => {
+    if (shape.shape === "circle") {
+      return Math.PI * Math.pow(shape.radius, 2);
+    } else {
+      return shape.width * shape.height;
+    }
+  };
+
+  const circleArea = calculateShapeArea({ shape: "circle", radius: 4 });
+  //   console.log(circleArea);
+
+  const rectangleArea = calculateShapeArea({
+    shape: "rectangle",
+    width: 8,
+    height: 6,
+  });
+  //   console.log(rectangleArea);
+
+  //
+}
