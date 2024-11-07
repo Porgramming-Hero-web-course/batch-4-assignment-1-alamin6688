320267cb5c30d867049b76473b3f4940b6cc1378
alamin6688 <alamin.kzs06@gmail.com>
2024-11-08 02:06:04 +0600

    Problem-5 Sloved

---
+++ src/problem-5.ts
@@ -0,0 +1,21 @@
+{
+  //
+  // Problem 05 Soluton:-
+
+  type Person = {
+    name: string;
+    age: number;
+  };
+
+  const person: Person = {
+    name: "Alamin",
+    age: 30,
+  };
+
+  function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+  }
+  const Output = getProperty(person, "name");
+  //   console.log(Output);
+  //
+}
